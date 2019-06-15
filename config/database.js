62b915922124695c76e021addaeada92df11d6c1
web-geek <Prem@India$$>
2019-06-15 15:23:43 +0530

    client

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require('mongoose')
 const url = 'mongodb://localhost:27017/my-notes'
-
+const connection_uri = `mongodb+srv://admin_67:admin_67@cluster0-pg9f0.mongodb.net/test?retryWrites=true&w=majority`
 mongoose.Promise = global.Promise
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(connection_uri, { useNewUrlParser: true })
     .then(() => {
         console.log('my-notes database: connected')
     })
