9c763ba411086ff1eb61ea6a9d10795659451720
web-geek <PremK.Lf@gmail.com>
2019-07-17 11:42:31 +0530

    initial commit

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
-const url = 'mongodb://localhost:27017/my-notes'
-const connection_uri = `mongodb+srv://admin_67:admin_67@cluster0-pg9f0.mongodb.net/test?retryWrites=true&w=majority`
+const connection_uri = 'mongodb://localhost:27017/my-notes'
+// const connection_uri = `mongodb+srv://admin_67:<password>@cluster0-pg9f0.mongodb.net/test?retryWrites=true&w=majority`
 mongoose.Promise = global.Promise
 
 mongoose.connect(connection_uri, { useNewUrlParser: true })
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const mongoose = require('./config/database')
 const cors = require('cors')
+
 const path = require("path");
 const port = process.env.PORT || 3001
 app.use(express.static(path.join(__dirname,"my-notes/build")))
@@ -13,14 +14,12 @@ const { categoryRouter } = require('./api/controllers/categoriesController')
 app.use(express.json())
 app.use(cors())
 
-
-
 app.use('/users', usersRouter)
 app.use('/notes', notesRouter)
 app.use('/categories', categoryRouter)
 
 app.get("*",(req,res)=>{
-    res.sendFile(path.join(__dirname + "/client/build/index.html"))
+    res.sendFile(path.join(__dirname + "/my-notes/build/index.html"))
 })
 
 app.listen(port, () => {
